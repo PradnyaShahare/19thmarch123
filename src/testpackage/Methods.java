233db29ddf81812ffc4143f20fec33c058fcfaf8
Pradnya Shahare <pradnyashahare29@gmail.com>
2022-10-10 17:47:09 +0530

    laxmi mate

--- src/testpackage/Methods.java
+++ src/testpackage/Methods.java
@@ -3,11 +3,12 @@ package testpackage;
 public class Methods {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		
 
 	}
 public static void add()
 {
-	
+	System.out.println("Hii I m going to change something");
 }
 }
